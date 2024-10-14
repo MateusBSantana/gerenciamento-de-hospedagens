2aea19d12d2019015f0c561f392e07c33b9f4597
ValdineideSchunk <valdineideschunk@gmail.com>
2024-10-14 13:45:09 -0300

    melhorando menu lateral

--- client/src/componentes/layout/MenuLateral/MenuLateral.jsx
+++ client/src/componentes/layout/MenuLateral/MenuLateral.jsx
@@ -1,67 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './MenuLateral.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faRightFromBracket, faHouse, faCalendar, faUserGroup, faMap, faGear, faUser } from '@fortawesome/free-solid-svg-icons'; // Certifique-se de que a capitalização está correta
+import { faHouse, faUser,faCalendar, faUserGroup, faMap, faGear, faChevronRight, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 function MenuLateral() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="d-flex flex-column vh-100 bg-primary text-white" style={{ width: '19%' }}>
-      <nav id="sidebar" className="d-flex flex-column flex-grow-1">
-        <div id="sidebar_content" className="p-3">
-          {/* Informações do usuário */}
-          <div id="user" className="mb-4 d-flex flex-column align-items-center">
-            <FontAwesomeIcon icon={faUser} style={{ color: "#ffffff" }} size="2x" />
-            <p id="user_infos" className="mt-2 text-center">
-              <span className="d-block">Ney Schunk</span>
-            </p>
-          </div>
-          {/* Itens do menu */}
-          <ul id="side_items" className="nav flex-column">
-            <li className="nav-item">
-              <a href="#" className="nav-link text-white">
-                <FontAwesomeIcon icon={faHouse} style={{ color: "#ffffff" }} size="2x" />
-                <span className="ms-2 fs-3">Home</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link text-white">
-                <FontAwesomeIcon icon={faCalendar} style={{ color: "#ffffff" }} size="2x" />
-                <span className="ms-2 fs-3">Reservas</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link text-white">
-                <FontAwesomeIcon icon={faUserGroup} style={{ color: "#ffffff" }} size="2x" />
-                <span className="ms-2 fs-3">Hóspedes</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link text-white">
-                <FontAwesomeIcon icon={faMap} style={{ color: "#ffffff" }} size="2x" />
-                <span className="ms-2 fs-3">Mapa</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link text-white">
-                <FontAwesomeIcon icon={faGear} style={{ color: "#ffffff" }} size="2x" />
-                <span className="ms-2 fs-3">Ajustes</span>
-              </a>
-            </li>
-          </ul>
-          {/* Botão para abrir/fechar o menu */}
-          <button id="open_btn" className="btn btn-link text-white mt-3">
-            <FontAwesomeIcon icon={faChevronRight} style={{ color: "#ffffff" }} size="2x" />
-          </button>
-        </div>
-        {/* Botão de logout */}
-        <div id="logout" className="mt-auto p-3">
-          <button id="logout_btn" className="btn btn-link text-white">
-            <FontAwesomeIcon icon={faRightFromBracket} style={{ color: "#ffffff" }} size="2x" />
-            <span className="ms-2 fs-3">Logout</span>
-          </button>
-        </div>
-      </nav>
+<div className={`d-flex flex-column bg-white vh-100 ${isOpen ? 'open-sidebar' : ''}`} id="sidebar" style={{ minWidth: isOpen ? '150px' : '60px', transition: 'min-width 0.6s', height: '100vh' }}>
+  <div id="sidebar_content" className="flex-grow-1 d-flex flex-column m-0 p-1" style={{ backgroundColor: '#006bb4' }}>
+    {/* Informações do usuário */}
+    <div id="user" className="d-flex align-items-center justify-content-start text-center mb-5 mt-3 ms-3">
+      <FontAwesomeIcon icon={faUser} style={{ fontSize: '25px', color: '#ffffff' }} />
+      <div id="user_infos" className="d-flex flex-column ms-0 align-items-start">
+      <span className="item-description text-white ms-0" style={{ fontSize: '12px' }}>Ney Schunk</span>
+      </div>
     </div>
+    {/* Itens do menu */}
+    <ul id="side_items" className="nav flex-column gap-2 flex-grow-1 align-items-start w-100">
+      <li className="nav-item side-item w-100">
+        <a href="#" className="nav-link text-white d-flex align-items-center gap-3 justify-content-start hover-effect w-100">
+          <FontAwesomeIcon icon={faHouse} style={{ fontSize: '25px', color: '#ffffff' }} />
+          <span className="item-description text-white">Home</span>
+        </a>
+      </li>
+      <li className="nav-item side-item w-100">
+        <a href="#" className="nav-link text-white d-flex align-items-center gap-3 justify-content-start w-100">
+          <FontAwesomeIcon icon={faCalendar} style={{ fontSize: '25px', color: '#ffffff' }} />
+          <span className="item-description text-white">Reservas</span>
+        </a>
+      </li>
+      <li className="nav-item side-item w-100">
+        <a href="#" className="nav-link text-white d-flex align-items-center gap-3 justify-content-start w-100">
+          <FontAwesomeIcon icon={faUserGroup} style={{ fontSize: '25px', color: '#ffffff' }} />
+          <span className="item-description text-white">Hóspedes</span>
+        </a>
+      </li>
+      <li className="nav-item side-item w-100">
+        <a href="#" className="nav-link text-white d-flex align-items-center gap-3 justify-content-start w-100">
+          <FontAwesomeIcon icon={faMap} style={{ fontSize: '25px', color: '#ffffff' }} />
+          <span className="item-description text-white">Mapa</span>
+        </a>
+      </li>
+      <li className="nav-item side-item w-100">
+        <a href="#" className="nav-link text-white d-flex align-items-center gap-3 justify-content-start w-100">
+          <FontAwesomeIcon icon={faGear} style={{ fontSize: '25px', color: '#ffffff' }} />
+          <span className="item-description text-white">Ajustes</span>
+        </a>
+      </li>
+      <li className="nav-item side-item mt-auto w-100">
+        <a href="#" className="nav-link text-white d-flex align-items-center gap-3 justify-content-start w-100">
+          <FontAwesomeIcon icon={faRightFromBracket} style={{ fontSize: '25px', color: '#ffffff' }} />
+          <span className="item-description text-white">Logout</span>
+        </a>
+      </li>
+    </ul>
+    {/* Botão para abrir/fechar o menu */}
+    <button id="open_btn" className="btn btn-link text-white d-flex justify-content-center align-items-center p-2" onClick={toggleMenu} style={{ position: 'absolute', top: '30px', right: '-10px', backgroundColor: '#4f46e5', borderRadius: '50%', width: '30px', height: '30px', transition: 'transform 0.3s' }}>
+      <FontAwesomeIcon icon={faChevronRight} style={{ fontSize: '20px' }} />
+    </button>
+  </div>
+</div>
+
+
+
   );
 }
 
