58878e4ef28577e18590819ad8fe35d0b080eeab
Mateus Barboza <m_bsantana@hotmail.com>
2024-11-26 19:49:09 -0300

    Formatando de forma correta a data de checkin e checkout

--- client/src/services/txtReserva.js
+++ client/src/services/txtReserva.js
@@ -2,9 +2,19 @@ import { saveAs } from "file-saver";
 
 export const txtReserva = (reservaData, nomeHospede, nomeAcomodacao) => {
 
+  function formatDateToDash(isoString) {
+    const date = new Date(isoString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 
-      
-    const reservaText = `
+  // Formatar datas de check-in e check-out
+  const formattedCheckin = formatDateToDash(reservaData.data_checkin);
+  const formattedCheckout = formatDateToDash(reservaData.data_checkout);
+
+  const reservaText = `
   ====================================
          🏨 DETALHES DA RESERVA 🏨
 ====================================
@@ -13,8 +23,8 @@ export const txtReserva = (reservaData, nomeHospede, nomeAcomodacao) => {
 ------------------------------------
 👤 **Hóspede:**           ${reservaData.nome_hospede}
 🏠 **Acomodação:**        ${reservaData.nome}
-📅 **Check-in:**          ${reservaData.data_checkin}
-📅 **Check-out:**         ${reservaData.data_checkout}
+📅 **Check-in:**          ${formattedCheckin}
+📅 **Check-out:**         ${formattedCheckout}
 👨‍👩‍👧‍👦 **Adultos:**          ${reservaData.numero_adulto}
 🧒 **Crianças:**          ${reservaData.numero_crianca}
 💵 **Valor da Diária:**    R$ ${reservaData.valor_diaria}
@@ -23,11 +33,11 @@ export const txtReserva = (reservaData, nomeHospede, nomeAcomodacao) => {
 📌 **Status da Reserva:** ${reservaData.status_reserva}
 
 ====================================
-       🌴 Pousada Hospeda Fácil 🌴
+       🌴 Hospeda Fácil 🌴
 ====================================
 
     `;
     
-    const blob = new Blob([reservaText], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, `Reserva_${nomeHospede}.txt`); // Nome do arquivo com base no nome do hóspede
+  const blob = new Blob([reservaText], { type: "text/plain;charset=utf-8" });
+  saveAs(blob, `Reserva_${nomeHospede}.txt`); // Nome do arquivo com base no nome do hóspede
 };
